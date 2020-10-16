0541eaa16632cc1f527d0cd62878abf5381dc53d
Dávid Kecskeméti <dxdy421@gmail.com>
2020-10-16 10:21:29 +0200

    update1

---
+++ config.js
@@ -0,0 +1,13 @@
+module.exports = {
+    'aws': {
+        'key': 'xxx',
+        'secret': 'xxx',
+        'ses': {
+            'from': {
+                // replace with actual email address
+                'default': '"Example.com" <noreply@example.com>', 
+            },
+            'region': 'xxx'    // e.g. us-west-2
+        }
+    }
+};
